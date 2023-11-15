d8d12901614dcf55a4f85886e9f4b327d93c1942
minchopm <5762871+minchopm@users.noreply.github.com>
2023-11-15 20:42:50 +0200

    Create log_level.go

---
+++ speech/log_level.go
@@ -0,0 +1,30 @@
+package speech
+
+type LogLevel int
+
+const (
+    LogLevelFatal LogLevel = iota
+    LogLevelError
+    LogLevelWarning
+    LogLevelInfo
+    LogLevelVerbose
+    // Add other levels as needed
+)
+
+// Helper function to convert LogLevel to C.OrtLoggingLevel
+func (l LogLevel) toCLogLevel() C.OrtLoggingLevel {
+    switch l {
+    case LogLevelFatal:
+        return C.ORT_LOGGING_LEVEL_FATAL
+    case LogLevelError:
+        return C.ORT_LOGGING_LEVEL_ERROR
+    case LogLevelWarning:
+        return C.ORT_LOGGING_LEVEL_WARNING
+    case LogLevelInfo:
+        return C.ORT_LOGGING_LEVEL_INFO
+    case LogLevelVerbose:
+        return C.ORT_LOGGING_LEVEL_VERBOSE
+    default:
+        return C.ORT_LOGGING_LEVEL_WARNING // Default level
+    }
+}
